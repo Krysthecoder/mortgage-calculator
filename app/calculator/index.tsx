3017113463f129e3da1d4273f3f8d98016a68b89
krysthecoder <krysthopher@gmail.comm>
2024-10-08 00:34:00 -0600

    reformated: (CalculatorContainer, App.tsx) prepared containers for future content

--- app/calculator/index.tsx
+++ app/calculator/index.tsx
@@ -1,7 +1,30 @@
 const CalculatorContainer = () => {
   return (
-    <div>
-      <h1>Hola Mundo</h1>
+    <div className="flex justify-center items-center w-9/12 h-96 bg-sky-200 shadow-2xl shadow-slate-950">
+      <div className="flex justify-center items-center w-5/6 h-auto">
+        <div className="bg-slate-400 w-6/12 p-20">
+          <div>
+            <h1>Mortgage Calculator</h1>
+            <button>Clear All</button>
+          </div>
+          <div>form</div>
+        </div>
+        <div className="w-6/12 bg-black p-20">
+          <div>
+            <h1>Your results</h1>
+            <p>
+              Your results are shosn based on the information provided. To
+              adjust the result, edit the form and click{' '}
+              <span className="font-semibold">calculate repayments </span>
+              again.
+            </p>
+          </div>
+          <div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import CalculatorContainer from './calculator';
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="flex items-center justify-center bg-red-50 h-screen">
       <CalculatorContainer />
     </div>
   );
